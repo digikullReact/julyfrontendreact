3f9c88c932cdae5c4a15b0d0b7a126b500082259
shubh <shubhamdixit863@gmail.com>
2022-12-13 18:46:03 +0530

    context api ,and usecontext

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,19 @@ import routes from "./routes";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import {RouterProvider,createBrowserRouter} from "react-router-dom"
+import DataContext from './context';
 const router=createBrowserRouter(routes)
+const Globalstate={
+     name:"John",
+     age:25
+
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
- 
-     <RouterProvider router={router} />
+     <DataContext.Provider value={Globalstate}>
+ <RouterProvider router={router} />
+     </DataContext.Provider>
+    
 
   
 )
--- src/pages/Something.jsx
+++ src/pages/Something.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React,{useContext} from 'react'
+import DataContext from '../context'
+
 
 const Something = () => {
+  const state=useContext(DataContext);
   return (
-    <div>Something</div>
+    <div>{
+       state.name
+      
+      }
+      {
+        state.age
+      }
+      </div>
   )
 }
 
