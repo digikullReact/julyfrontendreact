95577681ee839021531a2eca60511216d6ab05ce
shubh <shubhamdixit863@gmail.com>
2022-12-14 18:27:55 +0530

    usecallback

--- src/pages/Callbackhook.jsx
+++ src/pages/Callbackhook.jsx
@@ -1,22 +1,30 @@
 import React from 'react'
 import { useState } from 'react'
-import { useEffect } from 'react'
+import { useEffect,useCallback } from 'react'
 import ChildCb from './Childcb'
 
 const Callbackhook = () => {
   const [state,setState]=useState(0)
 
-  const changeData=()=>{
-    console.log("hello world")
 
-  }
   const changeState=()=>{
     setState(state+1);
   }
+
+  const changeData=useCallback(()=>{
+    console.log("hello world")
+  },[])
   return (
     <div>
       <h1>Parent State - {state}</h1>
-<ChildCb  changeData={changeData}/>
+      {
+        /**When the data will change then only ChildCb will re render other wise it wont if you memoize it
+         * <ChildCb data={state}  />
+         */
+      }
+
+<ChildCb changeData={changeData}  />
+
 
 <button onClick={changeState}>
   ChangeState Parent
--- src/pages/Childcb.jsx
+++ src/pages/Childcb.jsx
@@ -1,18 +1,23 @@
-import { useEffect } from 'react'
+import { useEffect,memo } from 'react'
 const ChildCb=(props)=>{
+  console.log("child re rendered")
 
   
     return (
       <div>
       <h1>Child Cb Component</h1>
+
+      <h1>Data From PArent Shown In child -{props.data}</h1>
+
+      <button onClick={props.changeData}>
+        Change Data Child
+      </button>
   
-  <button onClick={props.changeData}>
-    Child Button
-  </button>
+
   
       </div>
     )
   
   }
 
-  export default ChildCb;
+  export default memo(ChildCb);
